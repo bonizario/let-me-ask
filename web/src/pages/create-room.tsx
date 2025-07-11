5ed6db446717a6fa78dc4ea821216253d17e08ef
Gabriel Bonizário <56419828+bonizario@users.noreply.github.com>
2025-07-11 01:10:55 -0300

    feat: add create-room page

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,9 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
+import { ArrowRight, Loader2 } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { dayjs } from '@/lib/dayjs'
 
 type GetRoomsAPIResponse = {
   id: string
   name: string
+  questionsCount: number
+  createdAt: string
 }[]
 
 export function CreateRoom() {
@@ -17,18 +23,46 @@ export function CreateRoom() {
   })
 
   return (
-    <div>
-      <div>Create Room</div>
-      {isLoading && <div>Loading...</div>}
-      {data && (
-        <div className="flex flex-col gap-1">
-          {data.map((room) => (
-            <Link key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          ))}
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <div />
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Recent rooms</CardTitle>
+              <CardDescription>Quick access to recently created rooms</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-1 flex-col gap-3">
+              {isLoading && (
+                <Loader2 className="mx-auto size-6 animate-spin text-muted-foreground" />
+              )}
+              {data?.map((room) => (
+                <Link
+                  className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent/30"
+                  key={room.id}
+                  to={`/rooms/${room.id}`}
+                >
+                  <div className="flex flex-1 flex-col gap-1">
+                    <h3 className="font-medium">{room.name}</h3>
+
+                    <div className="fle items-center gap-2">
+                      <Badge variant="secondary">{dayjs(room.createdAt).fromNow()}</Badge>
+                      <Badge variant="secondary">
+                        {room.questionsCount} question{room.questionsCount !== 1 ? 's' : ''}
+                      </Badge>
+                    </div>
+                  </div>
+
+                  <span className="flex items-center gap-1 text-sm">
+                    Join <ArrowRight className="size-3" />
+                  </span>
+                </Link>
+              ))}
+            </CardContent>
+          </Card>
         </div>
-      )}
+      </div>
     </div>
   )
 }
